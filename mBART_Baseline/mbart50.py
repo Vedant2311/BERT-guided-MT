a5bda82f862ca6235dad77eda8f04afa62940709
Vedant2311 <skved004@gmail.com>
2023-11-21 14:57:11 -0800

    [Vedant] Added fine-tuning support in the mBART50 baseline script

--- mBART_Baseline/mbart50.py
+++ mBART_Baseline/mbart50.py
@@ -1,6 +1,11 @@
 from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+from transformers.optimization import AdamW
 from datasets import load_dataset
 from nltk.translate.bleu_score import corpus_bleu
+import torch
+
+# A flag to see whether we are fine-tuning the model or not
+fine_tune = False
 
 #initlaize model and tokenizer
 model_name = "facebook/mbart-large-50-many-to-many-mmt"
@@ -29,8 +34,57 @@ english_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 #print english translation
 print(english_text)
 
-#get a dataset from dataloader we create
-dataset = load_dataset("csv", data_files={"train": "/path/to/train.csv", "test": "/path/to/test.csv"})
+#get a dataset from dataloader we create.
+#TODO look into how can we load the dataset from the individual separate raw data files for Nepali and English
+dataset = load_dataset("csv", data_files={"train": "/path/to/train.csv", "test": "/path/to/test.csv", "valid": "/path/to/valid.csv"})
+
+'''
+Perform fine-tuning of mBART in case the flag is set appropriately
+You can check out references like these to understand the code better:
+    - https://colab.research.google.com/drive/1d2mSWLiv93X2hgt9WV8IJFms9dQ6arLn?usp=sharing
+    - https://github.com/huggingface/transformers/issues/23185#issuecomment-1537690520
+'''
+if fine_tune:
+    # Moving the model to CUDA
+    model = model.cuda()
+
+    # Get the train nepali and english sentences
+    nepali_sentences_train = dataset["train"]["nepali"]
+    english_references_train = dataset["train"]["english"]
+
+    # Set up the optimizer and training settings
+    optimizer = AdamW(model.parameters(), lr=1e-4)
+    model.train()
+
+    # Setting the number of epochs and batch-size
+    num_epochs = 20
+    batch_size = 32
+
+    # Getting the tokens corresponding to all the Input and output sentences and creating a tuple
+    train_set = []
+    raw_data_train = list(zip(nepali_sentences_train, english_references_train))
+    for data in raw_data_train:
+        source, target = data
+        # Test and check if we would need model_inputs.input_ids or if this should be fine
+        model_inputs = tokenizer(source, return_tensors="pt")
+        with tokenizer.as_target_tokenizer():
+            labels = tokenizer(target, return_tensors="pt").input_ids
+        train_set.append((model_inputs, labels))
+
+    # Creating a training batch using torch dataloader
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+    # Fine-tune for the specified number of epochs
+    for i in range(num_epochs):
+        for batch in train_loader:
+            model_inputs, labels = batch
+            model_inputs = model_inputs.cuda()
+            labels = labels.cuda()
+
+            optimizer.zero_grad()
+            output = model(**model_inputs, labels=labels) # forward pass
+            loss = output.loss
+            loss.backward() # Backward pass
 
 #get nepali sentences and english references
 nepali_sentences = dataset["test"]["nepali"]
