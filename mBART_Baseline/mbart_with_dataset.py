2c66650a92d77d361edc3ded58a2de0c9e92af40
Ryan Marr <r.marr747@gmail.com>
2023-11-28 21:30:50 -0800

    Update mbart_with_dataset.py
    I think this works

--- mBART_Baseline/mbart_with_dataset.py
+++ mBART_Baseline/mbart_with_dataset.py
@@ -1,57 +1,29 @@
-from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
-from transformers.optimization import AdamW
-from datasets import load_dataset
-from nltk.translate.bleu_score import corpus_bleu
-import torch
-from datasets import load_dataset
-from tqdm import tqdm
-
 # datasets
-DIR_PATH = "dataset"
-dataset_np = load_dataset("text", data_files= {"train": f"{DIR_PATH}/train_raw/train.ne_NP", "test": f"{DIR_PATH}/test_raw/test.ne_NP"})
-dataset_en = load_dataset("text", data_files={"train": f"{DIR_PATH}/train_raw/train.en_XX", "test": f"{DIR_PATH}/test_raw/test.en_XX"})
+DIR_PATH = "/workspace"
+BATCH_SIZE = 100
 
 # A flag to see whether we are fine-tuning the model or not
-fine_tune = False
+fine_tune = True
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-# Number of sentences to test on. Set to false value (e.g. None) to test on all sentences
-N = None
-
+print('init models')
 #initlaize model and tokenizer
 model_name = "facebook/mbart-large-50-many-to-many-mmt"
 model = MBartForConditionalGeneration.from_pretrained(model_name)
 tokenizer = MBart50TokenizerFast.from_pretrained(model_name)
-
-model.to(device)
-
-print(device)
-
-#english and nepali language codes
 tokenizer.src_lang = "ne_NP"
 tokenizer.tgt_lang = "en_XX"
+print('done')
 
-#text that says hi my name is trevor in nepali
-nepali_text = "नमस्ते, मेरो नाम त्रेवर हो।"
-
-#input tokens in nepali created by tokenizer
-input_ids = tokenizer(nepali_text, return_tensors="pt").input_ids.to(device)
-
-#find forced beginning of sentence token id
-forced_bos_token_id = tokenizer.lang_code_to_id["en_XX"]
-
-#generate english translation for "hi my name is trevor"
-outputs = model.generate(input_ids=input_ids, forced_bos_token_id=forced_bos_token_id, max_length=128)
-
-#decode generated english translation back into a sentence
-english_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-#print english translation
-print(english_text)
-
-#get a dataset from dataloader we create.
-#TODO look into how can we load the dataset from the individual separate raw data files for Nepali and English
+# #text that says hi my name is trevor in nepali
+# nepali_text = "नमस्ते, मेरो नाम त्रेवर हो।"
+# #input tokens in nepali created by tokenizer
+# input_ids = tokenizer(nepali_text, return_tensors="pt").input_ids
+# #find forced beginning of sentence token id
+# forced_bos_token_id = tokenizer.lang_code_to_id["en_XX"]
+# #generate english translation for "hi my name is trevor"
+# outputs = model.generate(input_ids=input_ids, forced_bos_token_id=forced_bos_token_id, max_length=128)
+# #decode generated english translation back into a sentence
+# english_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 '''
 Perform fine-tuning of mBART in case the flag is set appropriately
@@ -60,49 +32,39 @@ You can check out references like these to understand the code better:
     - https://github.com/huggingface/transformers/issues/23185#issuecomment-1537690520
 '''
 if fine_tune:
+    print('fine tuning')
     # Moving the model to CUDA
     model = model.cuda()
 
-    # Get the train nepali and english sentences
-    nepali_sentences_train = dataset_np["train"]
-    english_references_train = dataset_en["train"]
-
-    # Set up the optimizer and training settings
     optimizer = AdamW(model.parameters(), lr=1e-4)
     model.train()
-
-    # Setting the number of epochs and batch-size
-    num_epochs = 20
-    batch_size = 32
-
-    # Getting the tokens corresponding to all the Input and output sentences and creating a tuple
-    train_set = []
-    raw_data_train = list(zip(nepali_sentences_train, english_references_train))
-    for data in raw_data_train:
-        source, target = data
-        # Test and check if we would need model_inputs.input_ids or if this should be fine
-        model_inputs = tokenizer(source, return_tensors="pt")
-        with tokenizer.as_target_tokenizer():
-            labels = tokenizer(target, return_tensors="pt").input_ids
-        train_set.append((model_inputs, labels))
-
+    
     # Creating a training batch using torch dataloader
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
-
+    print('prepping data')
+    train_dataset = LanguageDataset(f'{DIR_PATH}/train.ne_NP.txt', f'{DIR_PATH}/train.en_XX.txt', BATCH_SIZE, tokenizer)
+    print(train_dataset)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    print('done')
+    
+    num_epochs = 20
     # Fine-tune for the specified number of epochs
     for i in range(num_epochs):
+        print(f'epoch {i+1} of {num_epochs}')
         # TODO: See if this is working. Otherwise we can just not use batches
+        # TODO: For loop not needed
         for batch in train_loader:
-            model_inputs, labels = batch
-            model_inputs = model_inputs.cuda()
-            labels = labels.cuda()
-
+            model_inputs = {k: v.to('cuda') for k, v in batch.items()}
+            labels = model_inputs['decoder_input_ids']
             optimizer.zero_grad()
+            # are args being passed correct???
             output = model(**model_inputs, labels=labels) # forward pass
             loss = output.loss
             loss.backward() # Backward pass
 
+# Test
 #get nepali sentences and english references
+dataset_np = load_dataset("text", data_files= {"train": f"{DIR_PATH}/train.ne_NP.txt", "test": f"{DIR_PATH}/test.ne_NP.txt"})
+dataset_en = load_dataset("text", data_files={"train": f"{DIR_PATH}/train.en_XX.txt", "test": f"{DIR_PATH}/test.en_XX.txt"})
 nepali_sentences = dataset_np["test"]
 english_references = dataset_en["test"]
 
@@ -110,29 +72,25 @@ english_references = dataset_en["test"]
 generated_translations = []
 
 #for each nepali sentence, generate english translation, and add to generated translations
-
-n = 0
-for nepali_sentence in tqdm(nepali_sentences):
-    if N and n == N:
-        break
-    n += 1
+for nepali_sentence in nepali_sentences:
     #input tokens in nepali created by tokenizer
-    input_ids = tokenizer(nepali_sentence['text'], return_tensors="pt").input_ids.to(device)
+    input_ids = tokenizer(nepali_sentence['text'], return_tensors="pt").input_ids.to('cuda')
 
     #find forced beginning of sentence token id
     forced_bos_token_id = tokenizer.lang_code_to_id["en_XX"]
 
     #generate english translation
-    outputs = model.generate(input_ids=input_ids, forced_bos_token_id=forced_bos_token_id, max_length=128)
+    outputs = model.generate(input_ids=input_ids, forced_bos_token_id=forced_bos_token_id, max_length=BATCH_SIZE)
 
     #decode generated english translation back into a sentence
     english_translation = tokenizer.decode(outputs[0], skip_special_tokens=True)
     
     # add english sentence to generated translations
     generated_translations.append(english_translation)
-    
+
 #calculate bleu score
 # map each sentence to a [ sentence.split() ]
+N = 0
 if N:
     references = [[reference.split()] for reference in english_references["text"][:N]]
 else:
